0b470e41f48da17c7402df994f428eacf57ca176
Seiji Naganuma <snaganumas@gmail.com>
2018-10-23 17:11:29 -0700

    add config

---
+++ README.md
@@ -0,0 +1,20 @@
+# eslint-config-curology
+An ESLint [Shareable Config](https://eslint.org/docs/developer-guide/shareable-configs)
+for use at [Curology](https://curology.com/).
+
+## Install
+Run
+
+```
+yarn add eslint-config-curology --dev
+```
+
+## Usage
+Assuming you already have ESLint configured, extend this configuration
+by adding this to your `.eslintrc` file:
+
+```
+{
+  "extends": "curology"
+}
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "prettier",
+    "prettier/react"
+  ],
+  parser: "babel-eslint",
+  plugins: ["react", "babel", "curology"],
+  rules: {
+    arrow-body-style: [2, "as-needed"],
+    arrow-parens: [2, "as-needed"],
+    class-methods-use-this: 0,
+    comma-dangle: [2, "always-multiline"],
+    consistent-return: 0,
+    curology/no-action-bound-decorator: 2,
+    curology/no-single-letter-variable: 2,
+    func-names: 0,
+    function-paren-newline: 0,
+    global-require: 0,
+    import/extensions: ["error", "never"],
+    import/no-unresolved: 0,
+    import/order: [2, {"groups": [["builtin", "external"], "internal", ["parent", "sibling"]], "newlines-between": "always"}],
+    import/prefer-default-export: 1,
+    import/no-extraneous-dependencies: 1,
+    indent: ["error", 2, { "SwitchCase": 1 }],
+    jsx-a11y/anchor-is-valid: 1,
+    jsx-a11y/click-events-have-key-events: 1,
+    jsx-a11y/img-redundant-alt: 1,
+    jsx-a11y/no-static-element-interactions: 1,
+    jsx-a11y/accessible-emoji: 1,
+    max-len: 0,
+    no-confusing-arrow: 0,
+    no-else-return: 1,
+    no-plusplus: 1,
+    no-static-element-interactions: 0,
+    no-underscore-dangle: 1,
+    object-curly-newline: 1,
+    object-property-newline: ["error", { "allowAllPropertiesOnSameLine": true }],
+    react/jsx-filename-extension: [1, { "extensions": [".js", ".jsx"] }],
+    react/no-array-index-key: 1,
+    react/require-default-props: 0,
+    space-before-function-paren: 0
+  },
+  settings: {
+    import/extensions: [".js", ".jsx"]
+  },
+};
